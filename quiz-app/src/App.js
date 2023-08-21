51365970327ed416a61d4ddf31d66603f6a8e2c9
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-08-21 14:20:00 +0700

    add more Quiz

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,17 +1,22 @@
-import {useState} from 'react'
+import {useState, createContext} from 'react'
 import Quiz from './components/Quiz'
 import Menu from './components/Menu'
 import Score from './components/Score'
+import './App.css'
+
+export const DataContext = createContext()
 
 function App() {
-  const [appState, setAppState] = useState('score')
+  const [appState, setAppState] = useState('menu')
   return ( 
-    <div className="App">
-      <h1>Web Develop QUIZ</h1>
-      {appState === 'menu' && <Menu/>}
-      {appState === 'quiz' && <Quiz/>}
-      {appState === 'score' && <Score/>}
-    </div>
+    <DataContext.Provider value={{appState, setAppState}}>
+      <div className="App">
+          <h1>Web Develop QUIZ</h1>
+          {appState === 'menu' && <Menu/>}
+          {appState === 'quiz' && <Quiz/>}
+          {appState === 'score' && <Score/>}
+      </div>
+    </DataContext.Provider>
   );
 }
 
--- quiz-app/src/components/Quiz.js
+++ quiz-app/src/components/Quiz.js
@@ -1,7 +1,18 @@
+import QuestionsData from '../data/QuestionsData'
+import { useState } from 'react'
+
 const Quiz = () =>{
+    console.log(QuestionsData);
+    const [current, setCurrent] = useState(3)
     return(
         <div className='quiz'>
-            <h1>Quiz Component</h1>
+            <h1>{QuestionsData[current].question}</h1>
+            <div className='choices'>
+                <button>{QuestionsData[current].A}</button>
+                <button>{QuestionsData[current].B}</button>
+                <button>{QuestionsData[current].C}</button>
+                <button>{QuestionsData[current].D}</button>
+            </div>
         </div>
     )
 }
