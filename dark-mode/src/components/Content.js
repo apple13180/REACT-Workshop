c51d8f4b2c15e610dd1b36799cb2cd6950a4938b
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-08-21 16:00:44 +0700

    Build Darkmode

---
+++ dark-mode/src/components/Content.js
@@ -0,0 +1,16 @@
+import light from '../images/light.svg'
+import dark from '../images/dark.svg'
+
+const Content = () => {
+    return (
+        <main>
+            <div>
+                <h1>Bunchana Kallayanapreecha</h1>
+                <p>Darkmode Darkmode</p>
+            </div>
+            <img src={light} alt="Logo"/>
+        </main>
+    )
+}
+
+export default Content
