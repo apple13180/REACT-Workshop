36a073e12d8793dc6b70b808791ee6ecadaac1a0
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-09-06 16:00:01 +0700

    Complete image-carousal

---
+++ image-carousel/src/components/ImageData.js
@@ -0,0 +1,20 @@
+const ImageData = [
+    {
+        title: "เที่ยวทะเล",
+        image: "https://cdn.pixabay.com/photo/2014/08/15/11/29/beach-418742_1280.jpg"
+    },
+    {
+        title: "ทานตะวัน",
+        image: "https://cdn.pixabay.com/photo/2016/07/23/00/12/sunflower-1536088_1280.jpg"
+    },
+    {
+        title: "ผีเสื้อ",
+        image: "https://cdn.pixabay.com/photo/2016/02/24/01/52/butterfly-1218884_1280.jpg"
+    },
+    {
+        title: "ต้มยำกุ้ง",
+        image: "https://cdn.pixabay.com/photo/2016/02/23/02/20/tom-yum-goong-1216817_1280.jpg"
+    }
+]
+
+export default ImageData
