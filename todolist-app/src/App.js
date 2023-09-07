082623c143ed4a44bc1a991a42d3ecb2a46714cd
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-09-07 14:11:58 +0700

    Set up project todolist

---
+++ todolist-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <section className='container'>
+        <h1>TodoList App</h1>
+        <form className='form-group'>
+          <input type='text' className='text-input'/>
+          <button type='submit' className='submit-btn'>เพื่มข้อมูล</button>
+        </form>
+    </section>
+  );
+}
+
+export default App;
