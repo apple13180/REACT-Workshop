fa0cb6242afe7757b6511828fb64afc9ed5f31fc
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-09-08 17:35:38 +0700

    Complete todolist

--- todolist-app/src/App.js
+++ todolist-app/src/App.js
@@ -1,13 +1,75 @@
+import { useState } from 'react';
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import List from './components/List';
+import Alert from './components/Alert';
 
 function App() {
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+
+  const [alert, setAlert] = useState({show:false, msg:'', type:''})
+  const [checkEditItem, setCheckEditItem] = useState(false)
+  const [editId, setEditId] = useState(null)
+
+  const submitData=(e)=>{
+    e.preventDefault()
+    if(!name){
+      setAlert({show:true, msg:"กรุณาป้อนข้อมูล", type:'error'})
+    } else if(checkEditItem && name){
+      const result = list.map((item)=>{
+        if(item.id === editId){
+          return {...item, title:name}
+        }
+        return item
+      })
+      setList(result)
+      setName('')
+      setCheckEditItem(false)
+      setEditId(null)
+      setAlert({show:true, msg:"แก้ใขข้อมูลเรียบร้อย", type:'success'})
+    } else {
+      const newItem = {
+        id: uuidv4(),
+        title: name
+      }
+      setList([...list, newItem])
+      setName('')
+      setAlert({show:true, msg:"บันทึกข้อมูลเรียบร้อย", type:'success'})
+    }
+  }
+
+  const removeItem =(id)=>{
+    const result = list.filter((item)=>item.id !== id)
+    setList(result)
+    setAlert({show:true, msg:"ลบข้อมูลเรียบร้อย", type:'error'})
+  }
+
+  const editItem=(id)=>{
+    setCheckEditItem(true)
+    setEditId(id)
+    const searchItem = list.find((item)=>item.id === id)
+    setName(searchItem.title)
+  }
+
   return (
     <section className='container'>
         <h1>TodoList App</h1>
-        <form className='form-group'>
-          <input type='text' className='text-input'/>
-          <button type='submit' className='submit-btn'>เพื่มข้อมูล</button>
+        {alert.show && <Alert {...alert} setAlert={setAlert} list={list}/>}
+        <form className='form-group' onSubmit={submitData}>
+          <div className='form-control'>
+            <input type='text' className='text-input' 
+            onChange={(e)=>setName(e.target.value)}
+            value={name}
+            />
+            <button type='submit' className='submit-btn'>{checkEditItem ? "แก้ใขข้อมูล" : "เพื่มข้อมูล"}</button>
+          </div>
         </form>
+        <section className='list-container'>
+          {list.map((data,index)=>{
+            return <List key={index} {...data} removeItem={removeItem} editItem={editItem}/>
+          })}
+        </section>
     </section>
   );
 }
