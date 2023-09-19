65bb651445233278ca148f78d2b55a290f006c36
Bunchana Kallayanapreecha <bird_sc@hotmail.co.th>
2023-09-19 16:16:56 +0700

    complete pagination

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import FoodComponent from './components/FoodComponent';
+import { useEffect, useState } from 'react'
+import MenuData from './data/MenuData';
 
 function App() {
+  const [foodData,setFoodData] = useState(MenuData)
+  const [dataInPage,setDataInPage] = useState([])
+  const [page,setPage] = useState(0)
+
+  const pagination=()=>{
+    const foodPerPage = 3 
+    const pages = Math.ceil(MenuData.length / foodPerPage)
+    console.log("จำนวนเลขหน้า = ", pages);
+
+    const newFood = Array.from({length:pages},(data,index)=>{
+      const start = index * foodPerPage
+      return MenuData.slice(start,start+foodPerPage)
+    })
+    return newFood
+  }
+  const handlePage=(index)=>{
+    setPage(index)
+  }
+
+  useEffect(()=>{
+    const paginate = pagination()
+    setDataInPage(paginate)
+    setFoodData(paginate[page])
+  },[page])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Foodcard | component</h1>
+        <div className='container'>
+            {foodData.map((data,index)=>{
+            return <FoodComponent key={index} {...data}/>
+            })}
+        </div>
+        <div className='pagination-container'>
+          {dataInPage.map((data,index)=>{
+            return (
+              <button key={index} onClick={()=>handlePage(index)} className={`page-btn ${index === page ? "active-btn" : null}`}>{index + 1}</button>
+            )
+          })}
+        </div>
     </div>
   );
 }
